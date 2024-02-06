14df85cced775dc71d450593eca72b1457f09a1a
Waldir Marquez Espiritu <waldirmarquezespiritu@Waldirs-MacBook-Pro.local>
2024-02-06 01:07:28 -0700

    Se corrigio problemas de consola

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,10 @@
-import { Buffer } from "buffer";
-import * as process from "process";
+import * as Buffer from '../node_modules/buffer/index';
+import 'zone.js';
 
 (window as any).global = window;
-(window as any).global.Buffer = Buffer;
-(window as any).process = process;
+(window as any).process = {};
+(window as any).process = window;
+(window as any).process.browser = true;
+(window as any).process.version = '';
+(window as any).process.version = {node:false};
+(window as any).global.Buffer = Buffer.Buffer;
