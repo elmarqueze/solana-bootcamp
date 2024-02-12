d809e57a878eb555897231e47cf1b8bacd186c41
Waldir Marquez Espiritu <waldirmarquezespiritu@Waldirs-MacBook-Pro.local>
2024-02-12 15:27:37 -0700

    Wallet conectada con mi proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { ShyftApiService } from './shyft-api.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { computedAsync } from 'ngxtension/computed-async';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, HdWalletMultiButtonComponent],
+  template: `
+    <header>
+      <h1 class="text-5xl text-center mb-4">Hola, soy Bob.</h1>
+      <div class="flex justify-center">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
+
+      @if (account()) {
+        <div>
+          <img src="" alt="">
+        </div>
+      }
+
+    </header>
+  `
 })
 export class AppComponent {
-  title = 'standalone-project';
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()), { requireSync: true },
+  )
 }
