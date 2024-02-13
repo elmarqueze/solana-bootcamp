5ca86e4ef53af08ed63a5c36d9392bc331ee571f
Waldir Marquez Espiritu <waldirmarquezespiritu@Waldirs-MacBook-Pro.local>
2024-02-13 02:41:48 -0700

    Páginas, secciones y estilos terminados

--- src/app/settings-page.component.ts
+++ src/app/settings-page.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FeaturesSectionComponent } from './features-section.component';
 
 @Component({
     selector: 'bob-settings-page',
-    template: `<h2>Settings</h2>`,
-    standalone: true
+    template: `
+        <section class="px-24 py-32 bg-gray bg-opacity-5">
+            <p class="text-center text-3xl">
+                SETTINGS.
+            </p>
+        </section>
+        <bob-features-section></bob-features-section>
+    `,
+    standalone: true,
+    imports: [FeaturesSectionComponent]
 })
 
 export class SettingsPageComponent {}
